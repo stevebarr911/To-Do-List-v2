8dcbf043e381bc1040562c176cc0eae90d8dd596
Steve Barr <SBarr@ezlinks.com>
2020-02-12 12:20:04 -0600

    Thrid Commit - resolve h10 heroku error crash

--- app.js
+++ app.js
@@ -151,6 +151,6 @@ if (port == null || port == "") {
   port = 3000;
 }
 
-app.listen(port, function() {
-  console.log("Server started successfully");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
